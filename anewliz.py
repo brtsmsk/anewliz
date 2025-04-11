ba6ab566974691fa4e6133349b65d5d0d24af3e5
Berat Şimşek <87765199+brtsmsk@users.noreply.github.com>
2025-04-11 15:58:45 +0300

    Update anewliz.py

--- anewliz.py
+++ anewliz.py
@@ -136,7 +136,7 @@ if st.button("🔍 Analiz Yap"):
                         st.pyplot(benzer["2.5 Üst"].value_counts().plot.pie(autopct="%1.1f%%", figsize=(3.3,3.3), ylabel="").figure)
                     with col3:
                     st.markdown("**KG Var**")
-                    st.pyplot(benzer["KG Var"].value_counts().plot.pie(autopct="%1.1f%%", figsize=(3.3,3.3), ylabel="").figure).plot.pie(autopct="%1.1f%%", figsize=(3.3,3.3), ylabel="").figure)}**")
+                    st.pyplot(benzer["KG Var"].value_counts().plot.pie(autopct="%1.1f%%", figsize=(3.3,3.3), ylabel="").figure)
             else:
                 st.info("Tahmin üretilemedi çünkü maç sonucu bilgisi eksik.")
         else:
